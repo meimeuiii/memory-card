d27e2ae42da127218642fa271044754b6d9f4e8b
meimeuiii <152019926+meimeuiii@users.noreply.github.com>
2023-12-30 19:28:24 +0200

    bybrik

--- window.py
+++ window.py
@@ -1,18 +1,35 @@
 from PyQt5.QtWidgets import*
 import database
+
+
 def menu_window():
     window = QDialog()
-    quest_lbl = QLabel("Введіть запитання")
-    right_ans_lbl = QLabel("Введіть правильну відповідь")
-    quest_edit = QLineEdit()
-    right_ans_edit = QLineEdit()
-    add_quest_btn = QPushButton
 
+
+    quest_lbl = QLabel("Введіть запитання")
+    right_ans_lbl = QLabel("Введіть правильну")
+    quest_edit = QLineEdit()
+    right_ans_lbl = QLineEdit()
+    add_question_btn = QLineEdit()
     main_line = QVBoxLayout()
-    h1 = QHBoxLayout()
+    h1=QHBoxLayout()
     h1.addWidget(quest_lbl)
     h1.addWidget(quest_edit)
     main_line.addLayout(h1)
 
+
+    def add_quest_func():
+        a = {"Запитання": quest_edit.text(),
+             "Правильна відповідь": "",
+             "Неправилна 1": "",
+             "Неправилна 2": "",
+             "Неправилна 3": "",
+
+             }
+        database.questions.append(a)
+
+
+    main_line.addWidget(add_quest_btn)
+    add_quest_btn.clicked.connect(add_quest_func)
     window.setLayout(main_line)
     window.exec()
